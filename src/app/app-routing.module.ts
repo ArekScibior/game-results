0f9a537b68709ce83bfdbe90d5cd3ce68d51912c
Ścibior Arek <scibior.arek@gmail.com>
2021-01-14 12:06:35 +0100

    [Detalis]: uzupelnienie danych na ekranie szczegolowym oraz tab z ostatnimi meczami.
    spinner - zmiana koloru.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DetailsComponent } from './details/details.component';
+import { MainViewComponent } from './main-view/main-view.component';
 
 
 const routes: Routes = [
+  { path: 'main', component: MainViewComponent },
   { path: 'details/:id', component: DetailsComponent },
 ];
 
--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,9 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { StorageCommonsService } from '../common/storage-commons.service';
+import { DataproviderService } from '../common/dataprovider.service';
+import { MatTableDataSource, MatPaginator, MatSort} from '@angular/material'
 import * as _ from 'underscore';
 import { ToastrService } from 'ngx-toastr';
+
+export interface LAST_GAMES {
+	date: string;
+	player1: string;
+	player2: string;
+	result: string;
+}
 
 @Component({
   selector: 'app-details',
@@ -11,17 +20,28 @@ import { ToastrService } from 'ngx-toastr';
   styleUrls: ['./details.component.scss']
 })
 export class DetailsComponent implements OnInit {
-
+  @ViewChild(MatPaginator, null) paginator: MatPaginator;
+  @ViewChild(MatSort, null) sort: MatSort;
   constructor(
     private route: ActivatedRoute,
     public store: StorageCommonsService,
     public toastr: ToastrService,
+    public dataprovider: DataproviderService,
+    
     ) { }
 
   currentPlayer = {}
   playerScores = []
+  loading = false
+  wins = {}
+	draws = 0;
+	
+	displayedColumns: string[] = ['date', 'player1', 'player2', 'result'];
+	dataSource = new MatTableDataSource<LAST_GAMES>();
+
   ngOnInit(): void {
     this.getPlayer();
+    this.get10LastMatches();
   }
   
   getPlayer(): void {
@@ -39,8 +59,39 @@ export class DetailsComponent implements OnInit {
         if (currentPlayerScore) { this.playerScores[idx] = currentPlayerScore }
       })
       this.currentPlayer = _.find(allPlayers, (v) => v.id == id)
-      console.log('currentPlayer',this.currentPlayer, this.playerScores)
     }
   }
+
+  get10LastMatches() {
+    let callback = (response) => {
+      if (!_.isEmpty(response)) {
+				let matches = []
+        matches = _.sortBy(response, 'date').reverse()
+        _.each(matches, (match) => {
+          match.result = match.player1Score + " - " + match.player2Score
+        })
+				this.dataSource = new MatTableDataSource<LAST_GAMES>(matches)
+				this.dataSource.paginator = this.paginator;
+				this.dataSource.sort = this.sort;
+				this.loading = false;
+			} else {
+				this.toastr.error('Brak danych dla wybranych graczy.')
+				this.loading = false;
+				return;
+			}
+    }
+
+    this.loading = true;
+    if (this.store.get('matches') && this.store.get('playerData') == this.currentPlayer.name) {
+      callback(this.store.get('matches'))
+    } else {
+      this.dataprovider.getLast10Matches({player1: this.currentPlayer.name, game: 'fifa21', numberMatches: 5}).subscribe(response => {
+        this.store.set('matches', response)
+        this.store.set('playerData', this.currentPlayer.name)
+        callback(response)
+      });
+    }
+		
+	}
 
 }
