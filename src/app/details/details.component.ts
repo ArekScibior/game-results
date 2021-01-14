df384abecd8d1e7c1e66672e7ea5c99ce3c402b5
Ścibior Arek <scibior.arek@gmail.com>
2021-01-14 20:18:00 +0100

    [Details-H2H]: dodanie obslugi h2h w zakladce - details-view.

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -7,7 +7,7 @@ import { MatTableDataSource, MatPaginator, MatSort} from '@angular/material'
 import * as _ from 'underscore';
 import { ToastrService } from 'ngx-toastr';
 
-export interface LAST_GAMES {
+export interface LAST_GAMESH2H {
 	date: string;
 	player1: string;
 	player2: string;
@@ -32,12 +32,16 @@ export class DetailsComponent implements OnInit {
 
   currentPlayer = {}
   playerScores = []
+  players = []
+  player2 = ""
   loading = false
   wins = {}
-	draws = 0;
+  draws = 0;
+  hideTable = true
 	
 	displayedColumns: string[] = ['date', 'player1', 'player2', 'result'];
-	dataSource = new MatTableDataSource<LAST_GAMES>();
+  dataSource = new MatTableDataSource<LAST_GAMESH2H>();
+  dataSourceH2H = new MatTableDataSource<LAST_GAMESH2H>();
 
   ngOnInit(): void {
     this.getPlayer();
@@ -47,21 +51,77 @@ export class DetailsComponent implements OnInit {
   getPlayer(): void {
     const allScores = this.store.get('score')
     const allPlayers = this.store.get('players')
+    this.players = _.pluck(allPlayers, 'name')
     const id = +this.route.snapshot.paramMap.get('id');
     if (_.isUndefined(allScores) || _.isUndefined(allPlayers)) {
       this.toastr.error('Przepraszamy, dane dot. graczy nie są kompletne.')
       return
     } else {
-      let existPlayerDetails = _.find(allPlayers, (v) => v.id == id)
       _.each(allScores, (v, idx) => {
-        let playerScores = {}
         let currentPlayerScore = _.find(v, function(player) { return player.idPlayer })
         if (currentPlayerScore) { this.playerScores[idx] = currentPlayerScore }
       })
       this.currentPlayer = _.find(allPlayers, (v) => v.id == id)
+      this.players = _.filter(this.players, (v) => v !== this.currentPlayer.name)
     }
   }
 
+	getMatchesH2H() {
+		this.hideTable = true;
+		this.loading = true;
+		this.dataprovider.getMatches({player1: this.currentPlayer.name, player2: this.player2, game: 'fifa21'}).subscribe(response => {
+			if (!_.isEmpty(response)) {
+				let matches = []
+				let player1 = this.currentPlayer.name
+				let wins = {
+					player1: 0,
+					player2: 0
+				}
+				let draws = 0;
+				_.each(response, function(v) {
+					let firstPlayerScore = ""
+					let secondPlayerScore = ""
+
+					if (v.player1 == player1) { 
+						firstPlayerScore = v.player1Score
+						secondPlayerScore = v.player2Score
+					} else {
+						firstPlayerScore = v.player2Score
+						secondPlayerScore = v.player1Score
+					}
+
+					if (firstPlayerScore > secondPlayerScore) {
+						wins.player1++
+					} else if (secondPlayerScore > firstPlayerScore) {
+						wins.player2++
+					} else if (firstPlayerScore == secondPlayerScore) {
+						draws++
+					}
+
+					let mappedObject = {
+						date: v.date,
+						player1: v.player1,
+						player2: v.player2,
+						result: v.player1Score + " - " + v.player2Score
+					}
+					matches.push(mappedObject)
+				})
+				matches = _.sortBy(matches, 'date').reverse()
+				this.wins = wins
+				this.draws = draws
+				this.dataSourceH2H = new MatTableDataSource<LAST_GAMESH2H>(matches)
+				this.dataSourceH2H.paginator = this.paginator;
+				this.dataSourceH2H.sort = this.sort;
+				this.hideTable = false;
+				this.loading = false;
+			} else {
+				this.toastr.error('Brak danych dla wybranych graczy.')
+				this.loading = false;
+				return;
+			}
+		});
+	}
+  
   get10LastMatches() {
     let callback = (response) => {
       if (!_.isEmpty(response)) {
@@ -70,7 +130,7 @@ export class DetailsComponent implements OnInit {
         _.each(matches, (match) => {
           match.result = match.player1Score + " - " + match.player2Score
         })
-				this.dataSource = new MatTableDataSource<LAST_GAMES>(matches)
+				this.dataSource = new MatTableDataSource<LAST_GAMESH2H>(matches)
 				this.dataSource.paginator = this.paginator;
 				this.dataSource.sort = this.sort;
 				this.loading = false;
